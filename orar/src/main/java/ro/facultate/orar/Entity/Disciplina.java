fea4d2dbd8780956964feb2c32c64cecd88dbac0
Alexandra <alexandraelen.7@gmail.com>
2026-07-10 11:20:54 +0300

    Add Disciplina

---
+++ orar/src/main/java/ro/facultate/orar/Entity/Disciplina.java
@@ -0,0 +1,25 @@
+package ro.facultate.orar.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "disciplina")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Disciplina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nume;
+
+    private Integer nrCredite;
+
+    private String cod;
+}
